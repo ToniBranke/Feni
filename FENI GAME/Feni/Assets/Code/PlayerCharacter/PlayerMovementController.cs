601b824645ec5912f61c9aa999b9716f19a76a84
Tbranke <tbranke@hs-mittweida.de>
2025-02-15 04:23:07 +0100

    Die Stimmen
    PlayerController theoretisch fast done aber man hat zZ als added Feature gebrochene Beine aber nen 29ft leap

--- FENI GAME/Feni/Assets/Code/PlayerCharacter/PlayerMovementController.cs
+++ FENI GAME/Feni/Assets/Code/PlayerCharacter/PlayerMovementController.cs
@@ -3,8 +3,21 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    public float accelleration; 
     public Rigidbody2D rb;
-    public float speed;
+    public BoxCollider2D CheckGroundCollider;
+    public LayerMask groundMask;
+
+    [Range (0f, 1f)]
+    public float groundDecay;
+    public float groundSpeed;
+    public float jumpSpeed;
+    public bool grounded; 
+
+    float xInput;
+    float yInput;
+    
+
     void start()
     {
 
@@ -12,17 +25,78 @@ public class PlayerMovementController : MonoBehaviour
         //transform.position = new Vector3(0, 0, 0);
     }
 
+    //Update wird mit jedem Frame gepulled deshalb soll man hier eher sachen rein packen die nicht zu krank viel Leistung ziehen also es muss nicht 60x pro sekunde überprüft werden, ob man zum Beispiel Friction hat
     void Update()
     {
         // Player movement mit Tastatur
-        float xInput = Input.GetAxis("Horizontal");
-        float yInput = Input.GetAxis("Vertical");
+        getInput();
+        HandleJump();
 
-        
-
-        Vector2 direction = new Vector2(xInput, yInput).normalized;
-        rb.linearVelocity = direction * speed;
+        //Bessere Steuerung überprüft wenn eine Bewegung auf der jeweiligen Achse durchgeführt wird und 
+        // dann macht packt der den Multiplikator von groundSpeed drauf sodass der Charakter sich tatsächlich bewegt
+       
+        //Normale Steuerung (falls alle stränge reißen kann man die benutzen)
+        //Vector2 direction = new Vector2(xInput, yInput).normalized;
+        //rb.linearVelocity = direction * groundSpeed;
+    }
+    void getInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
     }
 
+    //Tickrate mit der von Online Game zu vergleichen
+    void FixedUpdate()
+    {
+        CheckGround();
+        ApplyFriction();
+        // legt fest wie viel der Charakter abgebremst wird wenn er auf dem Boden ist (kann bei Eis Plattformen oder so nützlich sein)
+    }
 
+    void moveWithInput()
+    {
+       
+        // Bessere Steuerung überprüft wenn eine Bewegung auf der jeweiligen Achse durchgeführt wird und 
+        // dann macht packt der den Multiplikator von groundSpeed drauf sodass der Charakter sich tatsächlich bewegt
+    if (Mathf.Abs(xInput)> 0 ) 
+        {
+            float increment = xInput * accelleration;
+            float newSpeed = Mathf.Clamp(rb.linearVelocity.x + increment, -groundSpeed, groundSpeed);
+            rb.linearVelocity = new Vector2(newSpeed, rb.linearVelocity.y);
+            
+          //  float direction = Mathf.Sign(xInput);
+          //  transform.localscale = new Vector3(direction, 1, 1);
+        }
+
+       /* if(Mathf.Abs(yInput) > 0 && grounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, yInput * groundSpeed);
+        }
+        */
+
+        
+    }
+
+    void HandleJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+        }
+    }
+
+    void ApplyFriction()
+    {
+         // legt fest wie viel der Charakter abgebremst wird wenn er auf dem Boden ist (kann bei Eis Plattformen oder so nützlich sein)
+        if(grounded && xInput == 0 && rb.linearVelocity.y <= 0)
+        {
+            rb.linearVelocity *= groundDecay;
+        }
+    }
+
+// überprüft ob der Spieler auf dem Boden ist
+    void CheckGround()
+    {
+        grounded = Physics2D.OverlapAreaAll(CheckGroundCollider.bounds.min, CheckGroundCollider.bounds.max, groundMask).Length > 0;
+    }
 }
